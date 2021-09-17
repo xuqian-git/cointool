45ce85eb8414b270f12d6b8fbd66e17b39c8b609
xuqian <xuqian152@gmail.com>
2021-09-17 14:18:32 +0800

    first commit

---
+++ src/views/wallet/generate.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app-container">
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item label="数量">
+        <el-input v-model="formInline.num" placeholder="数量" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">生成</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        formInline: {
+          num: ''
+        },
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!')
+      },
+    },
+  }
+</script>
+
+<style scoped>
+  .el-form  {
+    text-align: center;
+    vertical-align: middle;
+  }
+</style>
